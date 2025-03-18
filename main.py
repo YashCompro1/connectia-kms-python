617fda2214dc2687d71fb4ba1b51bf4c6bbfeffc
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-03-18 12:09:08 +0530

    env related changes

--- main.py
+++ main.py
@@ -1,51 +1,71 @@
 from flask import Flask, request, jsonify, Response
 import json
 import base64
-from cryptography.hazmat.primitives.serialization import  load_pem_public_key
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
 import time
 import requests
 import os
-from flask_cors import CORS
+from typing import TypedDict
 
+class Payload(TypedDict):
+    userId: str
+    productId: str
+    env: str
+    expiry: int
+
+
+class Data(TypedDict):
+    payload: Payload
+    signature: str
+
+
+# Temp key to launch TG using current implementation. This may not be required in final implementation.
+# This is the secret key given to Cambridge from Connectia.
 CONNECTIA_KEY = os.environ.get("CONNECTIA_KEY", "default_secret_key")
 
 app = Flask(__name__)
-CORS(app)  # Allow requests from all origins
 
 @app.route('/', methods=['GET'])
 def home():
-    common()
     return jsonify({'message': 'working'})
+
+# Create a map for env variables and correspoding public keys
+key_maps = {
+    'thor': 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQ0lqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FnOEFNSUlDQ2dLQ0FnRUFybkRtd3lVaUZJcWQvSGhjTzl0KwpoVlI4RVlNOXFVamE4VzNRdGlKeHlQUkJaNjhtWmtDNDU4TGQzZTY3QS9DR1dSeStOb1g5WHBZN2FmOXg5ZVA2CnYzWjlCai9uaVZzTHkrdTQ0UGo1WWVBVkZwUHU2RnVuekZBZy9tSDJaalZCMFRUbDRoVmVScTNUQU04ZTloalgKb2o2N3hHSzNqVEM4Z3BNMzdXZXN0Q0o2cmhoVFk0anB2VytJalZkVjBhTWNLdXdUR0VJOWVxUnlzdS90djhzVwptZFRLVTVqZmswS3JBd1hMQjB5YnhNM2N5bGJNMDkzMEdHMmZaQisxZEdndkU0Y0xyNkorZm5YSnpoVUVlRkdxCko2REQzT25WbVBjZVBPQVJXVkNmTzN2SWpLc0JjUVhiUjdRWUVGRlR2UGg0cDcrZXVvcXVjWjBIbFpoYWllT0gKWHgrWTNwSERVcDZ0NmIza0ZVWG1EQ3NWa1lMaUdIMWttcERVRkJOSThub05JU3R6MHNVOXRLbmx6TmFFYjNsVAp6ZmtzNVo5OVQwc2xURk44UXdReHp4L2k1cWVPSll1U0Mwdjdud1QxL2xPN3VPM2paRTQyQTdGQTJwV1JqWDlKCmV1MjVvL2xpa2lGK3FocEI2N0JTT1J2bnFDRW9hanNZUitaZ3lwR0dQQm1ZOFpkcHlTbzhmc1JEbUhIdDJYdzAKVld6R0gyRDhDa1U5UWttQk02bDI1S0pPdHBveWRmV3hUNVhabzFJWk9URVd6SlFYU3BTTEUzekFwZ1FVSzRxQworcWdIY0ZCb0IrMmY1RDNyTnBJQUxCbXRIbEJVUFZwUi9QdndNN2FLQmZEeXVROEVDenVjdndmTmltL2VrMjNmCkpZVzZmc2xZdnNpaFF3WDI3QnQ3bEUwQ0F3RUFBUT09Ci0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQ==',
+    'qa': 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQ0lqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FnOEFNSUlDQ2dLQ0FnRUFybkRtd3lVaUZJcWQvSGhjTzl0KwpoVlI4RVlNOXFVamE4VzNRdGlKeHlQUkJaNjhtWmtDNDU4TGQzZTY3QS9DR1dSeStOb1g5WHBZN2FmOXg5ZVA2CnYzWjlCai9uaVZzTHkrdTQ0UGo1WWVBVkZwUHU2RnVuekZBZy9tSDJaalZCMFRUbDRoVmVScTNUQU04ZTloalgKb2o2N3hHSzNqVEM4Z3BNMzdXZXN0Q0o2cmhoVFk0anB2VytJalZkVjBhTWNLdXdUR0VJOWVxUnlzdS90djhzVwptZFRLVTVqZmswS3JBd1hMQjB5YnhNM2N5bGJNMDkzMEdHMmZaQisxZEdndkU0Y0xyNkorZm5YSnpoVUVlRkdxCko2REQzT25WbVBjZVBPQVJXVkNmTzN2SWpLc0JjUVhiUjdRWUVGRlR2UGg0cDcrZXVvcXVjWjBIbFpoYWllT0gKWHgrWTNwSERVcDZ0NmIza0ZVWG1EQ3NWa1lMaUdIMWttcERVRkJOSThub05JU3R6MHNVOXRLbmx6TmFFYjNsVAp6ZmtzNVo5OVQwc2xURk44UXdReHp4L2k1cWVPSll1U0Mwdjdud1QxL2xPN3VPM2paRTQyQTdGQTJwV1JqWDlKCmV1MjVvL2xpa2lGK3FocEI2N0JTT1J2bnFDRW9hanNZUitaZ3lwR0dQQm1ZOFpkcHlTbzhmc1JEbUhIdDJYdzAKVld6R0gyRDhDa1U5UWttQk02bDI1S0pPdHBveWRmV3hUNVhabzFJWk9URVd6SlFYU3BTTEUzekFwZ1FVSzRxQworcWdIY0ZCb0IrMmY1RDNyTnBJQUxCbXRIbEJVUFZwUi9QdndNN2FLQmZEeXVROEVDenVjdndmTmltL2VrMjNmCkpZVzZmc2xZdnNpaFF3WDI3QnQ3bEUwQ0F3RUFBUT09Ci0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQ==',
+    'rel': 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQ0lqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FnOEFNSUlDQ2dLQ0FnRUFybkRtd3lVaUZJcWQvSGhjTzl0KwpoVlI4RVlNOXFVamE4VzNRdGlKeHlQUkJaNjhtWmtDNDU4TGQzZTY3QS9DR1dSeStOb1g5WHBZN2FmOXg5ZVA2CnYzWjlCai9uaVZzTHkrdTQ0UGo1WWVBVkZwUHU2RnVuekZBZy9tSDJaalZCMFRUbDRoVmVScTNUQU04ZTloalgKb2o2N3hHSzNqVEM4Z3BNMzdXZXN0Q0o2cmhoVFk0anB2VytJalZkVjBhTWNLdXdUR0VJOWVxUnlzdS90djhzVwptZFRLVTVqZmswS3JBd1hMQjB5YnhNM2N5bGJNMDkzMEdHMmZaQisxZEdndkU0Y0xyNkorZm5YSnpoVUVlRkdxCko2REQzT25WbVBjZVBPQVJXVkNmTzN2SWpLc0JjUVhiUjdRWUVGRlR2UGg0cDcrZXVvcXVjWjBIbFpoYWllT0gKWHgrWTNwSERVcDZ0NmIza0ZVWG1EQ3NWa1lMaUdIMWttcERVRkJOSThub05JU3R6MHNVOXRLbmx6TmFFYjNsVAp6ZmtzNVo5OVQwc2xURk44UXdReHp4L2k1cWVPSll1U0Mwdjdud1QxL2xPN3VPM2paRTQyQTdGQTJwV1JqWDlKCmV1MjVvL2xpa2lGK3FocEI2N0JTT1J2bnFDRW9hanNZUitaZ3lwR0dQQm1ZOFpkcHlTbzhmc1JEbUhIdDJYdzAKVld6R0gyRDhDa1U5UWttQk02bDI1S0pPdHBveWRmV3hUNVhabzFJWk9URVd6SlFYU3BTTEUzekFwZ1FVSzRxQworcWdIY0ZCb0IrMmY1RDNyTnBJQUxCbXRIbEJVUFZwUi9QdndNN2FLQmZEeXVROEVDenVjdndmTmltL2VrMjNmCkpZVzZmc2xZdnNpaFF3WDI3QnQ3bEUwQ0F3RUFBUT09Ci0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQ==',
+    'prod1': 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQ0lqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FnOEFNSUlDQ2dLQ0FnRUFybkRtd3lVaUZJcWQvSGhjTzl0KwpoVlI4RVlNOXFVamE4VzNRdGlKeHlQUkJaNjhtWmtDNDU4TGQzZTY3QS9DR1dSeStOb1g5WHBZN2FmOXg5ZVA2CnYzWjlCai9uaVZzTHkrdTQ0UGo1WWVBVkZwUHU2RnVuekZBZy9tSDJaalZCMFRUbDRoVmVScTNUQU04ZTloalgKb2o2N3hHSzNqVEM4Z3BNMzdXZXN0Q0o2cmhoVFk0anB2VytJalZkVjBhTWNLdXdUR0VJOWVxUnlzdS90djhzVwptZFRLVTVqZmswS3JBd1hMQjB5YnhNM2N5bGJNMDkzMEdHMmZaQisxZEdndkU0Y0xyNkorZm5YSnpoVUVlRkdxCko2REQzT25WbVBjZVBPQVJXVkNmTzN2SWpLc0JjUVhiUjdRWUVGRlR2UGg0cDcrZXVvcXVjWjBIbFpoYWllT0gKWHgrWTNwSERVcDZ0NmIza0ZVWG1EQ3NWa1lMaUdIMWttcERVRkJOSThub05JU3R6MHNVOXRLbmx6TmFFYjNsVAp6ZmtzNVo5OVQwc2xURk44UXdReHp4L2k1cWVPSll1U0Mwdjdud1QxL2xPN3VPM2paRTQyQTdGQTJwV1JqWDlKCmV1MjVvL2xpa2lGK3FocEI2N0JTT1J2bnFDRW9hanNZUitaZ3lwR0dQQm1ZOFpkcHlTbzhmc1JEbUhIdDJYdzAKVld6R0gyRDhDa1U5UWttQk02bDI1S0pPdHBveWRmV3hUNVhabzFJWk9URVd6SlFYU3BTTEUzekFwZ1FVSzRxQworcWdIY0ZCb0IrMmY1RDNyTnBJQUxCbXRIbEJVUFZwUi9QdndNN2FLQmZEeXVROEVDenVjdndmTmltL2VrMjNmCkpZVzZmc2xZdnNpaFF3WDI3QnQ3bEUwQ0F3RUFBUT09Ci0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQ==',
+    'alpha': 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQ0lqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FnOEFNSUlDQ2dLQ0FnRUFybkRtd3lVaUZJcWQvSGhjTzl0KwpoVlI4RVlNOXFVamE4VzNRdGlKeHlQUkJaNjhtWmtDNDU4TGQzZTY3QS9DR1dSeStOb1g5WHBZN2FmOXg5ZVA2CnYzWjlCai9uaVZzTHkrdTQ0UGo1WWVBVkZwUHU2RnVuekZBZy9tSDJaalZCMFRUbDRoVmVScTNUQU04ZTloalgKb2o2N3hHSzNqVEM4Z3BNMzdXZXN0Q0o2cmhoVFk0anB2VytJalZkVjBhTWNLdXdUR0VJOWVxUnlzdS90djhzVwptZFRLVTVqZmswS3JBd1hMQjB5YnhNM2N5bGJNMDkzMEdHMmZaQisxZEdndkU0Y0xyNkorZm5YSnpoVUVlRkdxCko2REQzT25WbVBjZVBPQVJXVkNmTzN2SWpLc0JjUVhiUjdRWUVGRlR2UGg0cDcrZXVvcXVjWjBIbFpoYWllT0gKWHgrWTNwSERVcDZ0NmIza0ZVWG1EQ3NWa1lMaUdIMWttcERVRkJOSThub05JU3R6MHNVOXRLbmx6TmFFYjNsVAp6ZmtzNVo5OVQwc2xURk44UXdReHp4L2k1cWVPSll1U0Mwdjdud1QxL2xPN3VPM2paRTQyQTdGQTJwV1JqWDlKCmV1MjVvL2xpa2lGK3FocEI2N0JTT1J2bnFDRW9hanNZUitaZ3lwR0dQQm1ZOFpkcHlTbzhmc1JEbUhIdDJYdzAKVld6R0gyRDhDa1U5UWttQk02bDI1S0pPdHBveWRmV3hUNVhabzFJWk9URVd6SlFYU3BTTEUzekFwZ1FVSzRxQworcWdIY0ZCb0IrMmY1RDNyTnBJQUxCbXRIbEJVUFZwUi9QdndNN2FLQmZEeXVROEVDenVjdndmTmltL2VrMjNmCkpZVzZmc2xZdnNpaFF3WDI3QnQ3bEUwQ0F3RUFBUT09Ci0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQ==',
+    'hotfix': 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQ0lqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FnOEFNSUlDQ2dLQ0FnRUFybkRtd3lVaUZJcWQvSGhjTzl0KwpoVlI4RVlNOXFVamE4VzNRdGlKeHlQUkJaNjhtWmtDNDU4TGQzZTY3QS9DR1dSeStOb1g5WHBZN2FmOXg5ZVA2CnYzWjlCai9uaVZzTHkrdTQ0UGo1WWVBVkZwUHU2RnVuekZBZy9tSDJaalZCMFRUbDRoVmVScTNUQU04ZTloalgKb2o2N3hHSzNqVEM4Z3BNMzdXZXN0Q0o2cmhoVFk0anB2VytJalZkVjBhTWNLdXdUR0VJOWVxUnlzdS90djhzVwptZFRLVTVqZmswS3JBd1hMQjB5YnhNM2N5bGJNMDkzMEdHMmZaQisxZEdndkU0Y0xyNkorZm5YSnpoVUVlRkdxCko2REQzT25WbVBjZVBPQVJXVkNmTzN2SWpLc0JjUVhiUjdRWUVGRlR2UGg0cDcrZXVvcXVjWjBIbFpoYWllT0gKWHgrWTNwSERVcDZ0NmIza0ZVWG1EQ3NWa1lMaUdIMWttcERVRkJOSThub05JU3R6MHNVOXRLbmx6TmFFYjNsVAp6ZmtzNVo5OVQwc2xURk44UXdReHp4L2k1cWVPSll1U0Mwdjdud1QxL2xPN3VPM2paRTQyQTdGQTJwV1JqWDlKCmV1MjVvL2xpa2lGK3FocEI2N0JTT1J2bnFDRW9hanNZUitaZ3lwR0dQQm1ZOFpkcHlTbzhmc1JEbUhIdDJYdzAKVld6R0gyRDhDa1U5UWttQk02bDI1S0pPdHBveWRmV3hUNVhabzFJWk9URVd6SlFYU3BTTEUzekFwZ1FVSzRxQworcWdIY0ZCb0IrMmY1RDNyTnBJQUxCbXRIbEJVUFZwUi9QdndNN2FLQmZEeXVROEVDenVjdndmTmltL2VrMjNmCkpZVzZmc2xZdnNpaFF3WDI3QnQ3bEUwQ0F3RUFBUT09Ci0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQ=='
+}
 
 @app.route('/connectia', methods=['POST'])
 def connectia():
 
-    common()
-
     # Get token from request
     # Cambridge will be submitting a form with a base64 encoded token that contains payload and signature
-    token = request.form.get('token')
+    token: str = request.form.get('token')
 
     # Decode the base64 encoded token.
     decoded_data = base64.b64decode(token)
 
+    # Get the payload and signature from decoded token
+    data: Data = json.loads(decoded_data)
+    payload = data["payload"]
+    signature = data["signature"]
+
     # Get Public key from env vars
     # Public key will be generated and provided by cambridge
     # Public key will be base64 encoded
-    publicKey = 'LS0tLS1CRUdJTiBSU0EgUFVCTElDIEtFWS0tLS0tCk1JSUJDZ0tDQVFFQXoyMys1YklCT2J1cUZDL25pRlpUbnIyMFRaS0YwYWVKcnNGdEd3QkwzSjB5Tys0YnVMWkgKQ2E5aXNMYmV6KzRUT1pSV2VwaGVsYXMrVFM1SU0wMWVGanFBQ3FtZ1VraFBWSzlGallVN0k5aGlvdG9PdGN0RwpQZDVnOUZIL1YxT1MyNkhCci9Bd3pHTit4TDZTUy9VbmNCR1FIempuZHJEU3FtNFRrQXNNZU0wWnIvWmh1cmdYCjlMYmZoYkZRRytYQVprcUpzbU44V2JUZjM4dVE4TVgxWmVQZk1xeVdaczFhTXlkUzNvNXRUMTNvWGpVcG03S2oKd3dLbHY3ZlVRNG5yeDFrd015U1ZPdXZlbENKL1VVK1BGK2gyKzRBTFhvWXRsZDNrN1gyNUtmVElMMlFSdC9aYgpGWWYwMUtOTXFXZ2diRnhCU3ZPczc0bFlTaXZsYUhCbFl3SURBUUFCCi0tLS0tRU5EIFJTQSBQVUJMSUMgS0VZLS0tLS0K'
-    
+    publicKey = key_maps.get(payload.get('env'))
+
     # Decode the public key
     public_key_pem = base64.b64decode(publicKey)
 
-    # Load public key in the pem format 
+    # Load public key in the pem format
     public_key = load_pem_public_key(public_key_pem)
-
-    # Get the payload and signature from decoded token
-    data = json.loads(decoded_data)
-    payload = data["payload"]
-    signature = data["signature"]
 
     # Convert the payload json to string to verify it
     message = json.dumps(payload, separators=(',', ':')).encode()
@@ -68,10 +88,9 @@ def connectia():
         productId = payload['productId']
 
         # expiry from cambridge is recieved in ms, whilst time.time() returns timestamp in seconds.
-        # Use correct conversion and logic to return error incase expiry is passed. 
-        if time.time()*1000>expiry :
+        # Use correct conversion and logic to return error incase expiry is passed.
+        if time.time()*1000 > expiry:
             return jsonify({"error": "time expired"}), 401
-
 
         # If everything is correct, then launch connectia test generator
         url = "https://c1.conectia.es/login.php"
@@ -88,44 +107,8 @@ def connectia():
         return Response(response.text, status=response.status_code, content_type=response.headers.get("Content-Type", "text/plain"))
 
     except Exception as e:
+        print(e)
         return jsonify({"error": "token invalid"}), 401
-
-
-def common():
-    print ('-'*40)
-    headers = dict(request.headers)
-
-    print ('origin', request.origin, request.headers.get('origin'))
-    
-    # Get query parameters (GET requests)
-    query_params = request.args.to_dict()
-
-    # Get form data (POST requests)
-    form_data = request.form.to_dict()
-
-    # Get JSON data (POST requests with application/json)
-    json_data = request.get_json(silent=True)
-
-    # Get raw body (if needed)
-    raw_body = request.data.decode("utf-8")
-
-    # Get request method and remote address
-    method = request.method
-    ip_address = request.remote_addr
-
-    # Print everything in the console
-    print(f"Headers: {headers}")
-    print(f"Query Params: {query_params}")
-    print(f"Form Data: {form_data}")
-    print(f"JSON Data: {json_data}")
-    print(f"Raw Body: {raw_body}")
-    print(f"Method: {method}")
-    print(f"IP Address: {ip_address}")
-    print ('-'*40)
-
 
 if __name__ == '__main__':
     app.run()
-
-
-        
